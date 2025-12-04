466227048b3da14d293ef6d07c1206ab3e87f274
Anthony Dao <tony_bazzite@DESKTOP-89V1U9L-66.home>
2025-12-04 00:27:09 -0800

    removed vision cone and inventory for redesign

--- src/gameplay/gameplay.rs
+++ src/gameplay/gameplay.rs
@@ -12,18 +12,15 @@ impl Plugin for GameplayPlugin {
         .insert_resource(MousePos {
             position: Vec2::new(0.0, 0.0),
         })
-        .add_plugins(InventoryPlugin)
-        .add_message::<PickupMessage>()
         .add_systems(
             Startup,
-            (init_camera, init_environment, init_litter_text, spawn_player, spawn_objects),
+            (init_camera, init_environment, spawn_player),
         )
         .add_systems(
             Update,
             (
                 update_camera,
                 get_mouse_position,
-                move_cursor_text.after(get_mouse_position),
                 rotate_aim.after(get_mouse_position),
                 movement,
                 run,
@@ -31,9 +28,6 @@ impl Plugin for GameplayPlugin {
                 update_stamina,
                 update_health,
                 restore_stamina,
-                update_fog,
-                apply_fog_visuals,
-                draw_visibility,
             ),
         );
         
